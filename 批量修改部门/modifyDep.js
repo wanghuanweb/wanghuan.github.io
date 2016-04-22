04c56278decb559127ee723a2bb39e72a6910435
wang huan <15611002722@163.com>
2016-04-22 21:20:56 +0800

    modify

--- 批量修改部门/modifyDep.js
+++ 批量修改部门/modifyDep.js
@@ -14,35 +14,38 @@ function init() {
   }
 
   addButton.addEventListener("click",function() {
+    if(depModify.belongDep.children.length > 0){
+        alert("最多选一个部门");
+        return;
+    }
     depModify.otherDepDel();
     depModify.targetNode.style.height = "30px";
-    if(depModify.belongDep.children.length % 2 === 0) {
-      depModify.targetNode.style.background = "#ffffff";
-    }else {
-      depModify.targetNode.style.background = "#2189bf";
+    if(depModify.targetNode !== depModify.otherDep){
+        depModify.belongDep.appendChild(depModify.targetNode);
     }
-    depModify.belongDep.appendChild(depModify.targetNode);
+
   });
 
   delButton.addEventListener("click",function() {
     depModify.belongDepDel();
     depModify.targetNode2.style.height = "30px";
-    if(depModify.otherDep.children.length % 2 === 0) {
-      depModify.targetNode2.style.background = "#ffffff";
-    }else {
-      depModify.targetNode2.style.background = "#2189bf";
+    if(depModify.targetNode2 !== depModify.belongDep){
+        depModify.otherDep.appendChild(depModify.targetNode2);
     }
-    depModify.otherDep.appendChild(depModify.targetNode2);
   });
 
   table1.addEventListener("click",function(event) {
     depModify.targetNode = event.target;
-    event.target.style.background = "#99ccdd";
+    if(event.target !== depModify.belongDep){
+        event.target.style.background = "#99ccdd";
+    }
   });
 
-  table1.addEventListener("click",function(event) {
+  table2.addEventListener("click",function(event) {
     depModify.targetNode2 = event.target;
-    event.target.style.background = "#99ccdd";
+    // if(event.target !== depModify.otherDep){
+    //     event.target.style.background = "#99ccdd";
+    // }
   });
 
 }
@@ -51,26 +54,33 @@ function DepModify() {
   this.deps = ["研发部","产品部","客服部","市场部","售后部","销售部","交互部","研发部","产品部","客服部","市场部","售后部","销售部","交互部","研发部","产品部","客服部","市场部","售后部","销售部","交互部"];
   this.belongDeps = [];
   this.otherDep =  document.getElementById('otherDepTable');
+  this.len = otherDep.children[1].children.length;
   this.belongDep =  document.getElementById('belongDepTable');
   this.targetNode = this.otherDep;
   this.targetNode2 = this.belongDep;
 }
 
 DepModify.prototype.otherDepDel = function() {
-  if(this.targetNode && this.targetNode == this.otherDep) {
-    alert("please choose a dep");
-  }else{
-    this.targetNode.parentNode.removeChild(this.targetNode);
-  }
-};
+	  if(this.targetNode && this.targetNode == this.otherDep) {
+	    alert("please choose a dep");
+
+	    return;
+	  }else{
+		this.len--;
+	    this.targetNode.parentNode.removeChild(this.targetNode);
+	    // for(var i = 0;i < this.len;i++) {
+	    // 	if(i%2 === 0){
+	    // 		this.otherDep.children[1].children[i].style = "#CCCCCC";
+	    // 	}
+	    // }
+	  }
+	};
 
 DepModify.prototype.belongDepDel = function() {
-  if(this.targetNode2 && this.targetNode2 == this.belongDep) {
-    alert("please choose a dep");
-  }else{
-    this.targetNode2.parentNode.removeChild(this.targetNode2);
-  }
-};
-// DepModify.prototype.updateDeps = function() {
-//
-// }
+	  if(this.targetNode2 && this.targetNode2 == this.belongDep) {
+	    alert("please choose a dep");
+	    return;
+	  }else{
+	    this.targetNode2.parentNode.removeChild(this.targetNode2);
+	  }
+	};
