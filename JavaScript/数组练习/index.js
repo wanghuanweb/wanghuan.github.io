747677d03beb85a814baa74b1bb61684231982b6
wang huan <15611002722@163.com>
2016-05-07 17:10:42 +0800

    add canvas

--- JavaScript/数组练习/index.js
+++ JavaScript/数组练习/index.js
@@ -1,10 +1,35 @@
-var numTotal = factorial(5);
-
-function factorial(num){
-	if(num <=1 ){
-		return 1;
-	}else {
-		return num * arguments.callee(num-1);
-	}
+function Animal(){
+	this.species = "动物";
 }
-console.log(numTotal);
+
+function Cat(name,color){
+	this.name = name;
+	this.color = color;
+}
+
+Cat.prototype = new Animal();
+Cat.prototype.constructor = Cat;
+var cat1 = new Cat("大毛","黄色");
+alert(cat1.species); // 动物
+
+var Chinese = {
+　　　　nation:'中国'
+　　};
+var Doctor ={
+　　　　career:'医生'
+　　};
+function extendCopy(p) {
+
+　　　　var c = {};
+
+　　　　for (var i in p) {
+　　　　　　c[i] = p[i];
+　　　　}
+
+　　　　c.uber = p;
+
+　　　　return c;
+　　}
+var Doctor = extendCopy(Chinese);
+Doctor.career = '医生';
+alert(Doctor.nation); // 中国
