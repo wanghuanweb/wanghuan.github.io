3a11da54d87b9fd1def01b6fa30b2e4b5c322a23
wanghuanweb <15611002722@163.com>
2017-03-09 18:57:57 +0800

    js

--- 模块化测试/js/main.js
+++ 模块化测试/js/main.js
@@ -1,9 +1,16 @@
 require.config({
 　　　　paths: {
 　　　　　　"jquery": "jquery-3.1.1.min",
+            "math":"math"
 　　　　}
 　　});
 require(['jquery'], function ($){
 　　　　// some code here
-　　　　$(".bottom").html("hhh");
+            $(".btn").click(function(){
+                console.log('ok');
+                $(".content").html('成功！')
+            });
+　　});
+require(['math'], function (math){
+　　　　alert(math.add(1,1));
 　　});
