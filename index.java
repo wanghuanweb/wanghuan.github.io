c105b2bedc56803366ce81f4b5ee64cb53584264
wanghuanweb <15611002722@163.com>
2017-04-23 22:48:56 +0800

    js

--- index.java
+++ index.java
@@ -26,3 +26,57 @@ public class Main {
         System.out.println(ans);
 	}
 }
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+		Set<Integer> set = new HashSet<Integer>(m);
+
+		for(int i = 0;i < m;i++){
+            set.add(sc.nextInt());
+		}
+
+        int n = sc.nextInt();
+		List<Integer> ans = new ArrayList<Integer>();
+
+		for(int i = 0;i < n;i++){
+            int k = sc.nextInt();
+			if(set.contains(k)){
+				ans.add(k);
+			}
+		}
+
+		for(Integer num:ans){
+			System.out.print(num+" ");
+		}
+	}
+}
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		long uid=sc.nextLong();
+		Set<Long> set=new HashSet<Long>();
+		while(uid!=0){
+			set.add(uid);
+			uid=sc.nextLong();
+		}
+		System.out.println(set.size());
+
+	}
+
+}
